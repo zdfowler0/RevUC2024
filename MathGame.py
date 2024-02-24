f6a2cc07b60a23809b4a5a568a66b6e544026a88
zdfowler0 <155563410+zdfowler0@users.noreply.github.com>
2024-02-24 15:37:56 -0500

    Update MathGame.py
    Added functions to create problems of each level and a function to call those functions.

--- MathGame.py
+++ MathGame.py
@@ -1,42 +1,93 @@
+# import libraries
 import random
 import math
 
-num1 = random.randint(1, 9)
-num2 = random.randint(1, 9)
+# list of valid operations
+operations = ["+", "-", "*", "/", "^", "^-"]
 
-operation = ["+", "-", "*"]
+# function to generate level 1 problems
+def generate_level_1_problem(lower=0, upper=5):
+    num1 = random.randint(lower, upper)
+    num2 = random.randint(lower, upper)
 
-op = random.randint(0, 2)
+    print(f"{num1} + {num2}")
 
-if(operation[op] == "+"):
     result = num1 + num2
-elif(operation[op] == "-"):
+    print(result)
+
+# function to generate level 2 problems
+def generate_level_2_problem(lower=0, upper=5):
+    num1 = random.randint(lower, upper)
+    num2 = random.randint(lower, num1)
+
+    print(f"{num1} - {num2}")
+
     result = num1 - num2
-elif(operation[op] == "*"):
-    result = num1 * num2
+    print(result)
 
-score = 0
+# function to generate level 3 problems
+def generate_level_3_problem():
+    # determine operation
+    operation = random.randint(0, 1)
+    
+    if(operation == 0): # Addition
+        generate_level_1_problem()
+    else: # Subtraction
+        generate_level_2_problem()
 
-ans = int(input(f"Please enter {num1} {operation[op]} {num2}: "))
+# function to generate level 4 problems
+def generate_level_4_problem():
+    # determine operation
+    operation = random.randint(0, 1)
+    
+    lower = 0
+    upper = 20
 
-while(ans == result):
-    print("Correct!")
-    score += 1
+    if(operation == 0): # Addition
+        upper /= 2
+        generate_level_1_problem(lower=lower, upper=upper)
+    else: # Subtraction
+        generate_level_2_problem(lower=lower, upper=upper)
 
-    num1 = random.randint(1, 9)
-    num2 = random.randint(1, 9)
+# function to generate level 5 problems
+def generate_level_5_problem():
+    # determine operation
+    operation = random.randint(0, 1)
+    
+    # bounds for operation
+    lower = 0
+    upper = 100
 
-    op = random.randint(0, 2)
+    if(operation == 0): # Addition
+        upper /= 2
+        generate_level_1_problem(lower=lower, upper=upper)
+    else: # Subtration
+        generate_level_2_problem(lower=lower, upper=upper)
 
-    if(operation[op] == "+"):
-        result = num1 + num2
-    elif(operation[op] == "-"):
-        result = num1 - num2
-    elif(operation[op] == "*"):
-        result = num1 * num2
+# generate a problem with a specified level
+def generate_problem(level):
+    print(f"Level {level} problem:")
+    
+    # determine which level problem to generate
+    match level:
+        case 1:
+            generate_level_1_problem()
+        case 2:
+            generate_level_2_problem()
+        case 3:
+            generate_level_3_problem()
+        case 4:
+            generate_level_4_problem()
+        case 5:
+            generate_level_5_problem()
+        case _:
+            print("ERROR: NO LEVEL 0 PROBLEMS")
 
-    ans = int(input(f"Please enter {num1} {operation[op]} {num2}: "))
+# Test start
+print("Welcome to the Math Game!")
+print("Mode: High Score\n")
 
-
-print("Wrong :/")
-print(f"Score: {score}")
+# Generate 5 problems levels 1 - 5
+for i in range(1, 6):
+    generate_problem(i)
+    print("")
