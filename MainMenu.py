e24d9164b837594f1866f0f8b253737fae5d3762
zdfowler0 <155563410+zdfowler0@users.noreply.github.com>
2024-02-24 23:44:56 -0500

    Update MainMenu.py
    Added some comments

--- MainMenu.py
+++ MainMenu.py
@@ -1,3 +1,10 @@
+'''
+This is the main menu for the math game.
+This game revolves around answering math questions
+in fun and unique ways. This code contains the main
+menu for the game, which is where the user can
+select the mode that they would like to play.
+'''
 #import python files
 import focus_mode
 import time_attack
@@ -13,22 +20,22 @@ import tkinter.font as font
 
 root = Tk()
 
-#fonts
+# define fonts
 titleFont = font.Font(size=80)
 myFont = font.Font(family = "Comic Sans MS")
 buttonFonts = font.Font(size=30)
 
-#create window
+# create window
 root.geometry("1520x800+1+1")
 root.title("Main Menu")
 
-#title
+# title
 welcometothe = Label(root, text="Welcome to the ", font = titleFont)
 welcometothe.pack()
 mathgame = Label(root, text="Math Game!", font = titleFont)
 mathgame.pack()
  
-#functions
+# functions for each game mode
 def play_focus_mode():
     focus_mode.focus_mode()
 def play_time_attack():
@@ -37,13 +44,15 @@ def play_high_score():
     high_score
 
 
-#button
+# button to start focus mode
 focusmode = Button(root, text ="Focus Mode",font = buttonFonts,fg = 'white', bg = 'blue',command = play_focus_mode)
 focusmode.place(x=300,y=300)
 
+# button to start time attack
 timeattack = Button(root, text = "Time Attack", font = buttonFonts, fg = 'white', bg = 'red', command = play_time_attack)
 timeattack.place(x= 600, y=300)
 
+# button to start highscore
 highscore = Button(root, text = "High Score", font = buttonFonts, fg = 'white', bg = 'green', command = play_high_score)
 highscore.place(x=900, y=300)
 
